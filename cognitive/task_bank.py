f37b638e74a51d93df7f1df2d74f975d07ba6188
XIAOXUAN LEI <xiaoxuanlei@XIAOXUANs-iMac.local>
2021-12-21 20:51:26 -0500

    Initial commit

---
+++ cognitive/task_bank.py
@@ -0,0 +1,56 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""A bank of available tasks."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import OrderedDict
+import random
+import tensorflow as tf
+
+from cognitive import stim_generator as sg
+from cognitive import task_generator as tg
+from cognitive.task_generator import Task
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string('task_family', 'all', 'name of the task to be trained')
+
+
+class GoShape(Task):
+  """Go to shape X."""
+
+  def __init__(self):
+    shape1 = sg.random_shape()
+    when1 = sg.random_when()
+    objs1 = tg.Select(shape=shape1, when=when1)
+    self._operator = tg.Go(objs1)
+
+  @property
+  def instance_size(self):
+    return sg.n_random_shape() * sg.n_random_when()
+
+
+task_family_dict = OrderedDict([
+    ('GoShape', GoShape),
+])
+
+
+def random_task(task_family):
+  """Return a random question from the task family."""
+  return task_family_dict[task_family]()
---
+++ main.py
@@ -0,0 +1,133 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Script for generating a COG dataset"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import errno
+import functools
+import gzip
+import itertools
+import json
+import multiprocessing
+import os
+import random
+import shutil
+import traceback
+
+import numpy as np
+import tensorflow as tf
+
+from cognitive import stim_generator as sg
+import cognitive.task_bank as task_bank
+
+FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_integer('max_memory', 3, 'maximum memory duration')
+tf.flags.DEFINE_integer('max_distractors', 1, 'maximum number of distractors')
+tf.flags.DEFINE_integer('epochs', 4, 'number of epochs')
+tf.flags.DEFINE_boolean('compress', True, 'whether to gzip the files')
+tf.flags.DEFINE_integer('examples_per_family', 2,
+                     'number of examples to generate per task family')
+tf.flags.DEFINE_string('output_dir', '/tmp/cog',
+                    'Directory to write output (json or tfrecord) to.')
+tf.flags.DEFINE_integer('parallel', 0,
+                     'number of parallel processes to use. Only training '
+                     'dataset is generated in parallel.')
+
+
+try:
+  range_fn = xrange  # py 2
+except NameError:
+  range_fn = range  # py 3
+
+
+def get_target_value(t):
+  # Convert target t to string and convert True/False target values
+  # to lower case strings for consistency with other uses of true/false
+  # in vocabularies.
+  t = t.value if hasattr(t, 'value') else str(t)
+  if t is True or t == 'True':
+    return 'true'
+  if t is False or t == 'False':
+    return 'false'
+  return t
+
+
+def generate_example(max_memory, max_distractors, task_family, epochs):
+  #random.seed(1)
+  task = task_bank.random_task(task_family)
+
+  # To get maximum memory duration, we need to specify the following average
+  # memory value
+  avg_mem = round(max_memory/3.0 + 0.01, 2)
+  if max_distractors == 0:
+      objset = task.generate_objset(n_epoch=epochs,
+                                    average_memory_span=avg_mem)
+  else:
+      objset = task.generate_objset(n_epoch=epochs,
+                                    n_distractor=random.randint(1, max_distractors),
+                                    average_memory_span=avg_mem)
+  # Getting targets can remove some objects from objset.
+  # Create example fields after this call.
+  targets = task.get_target(objset)
+
+  example = {
+      'family': task_family,
+      'epochs': epochs,  # saving an epoch explicitly is needed because
+                         # there might be no objects in the last epoch.
+      'question': str(task),
+      'objects': [o.dump() for o in objset],
+      'answers': [get_target_value(t) for t in targets]
+  }
+  return example, objset, task
+
+
+
+def log_exceptions(func):
+  @functools.wraps(func)
+  def wrapped_func(*args, **kwargs):
+    try:
+      return func(*args, **kwargs)
+    except BaseException as e:
+      print('Exception in ' + func.__name__)
+      traceback.print_exc()
+      raise e
+  return wrapped_func
+
+
+def main(argv):
+    # go shape: point at last sth
+    max_distractors = 0
+    max_memory = 1
+    epochs = 4
+    families = list(task_bank.task_family_dict.keys())
+    task_family = families[0]
+    example, objset, task = generate_example(max_memory, max_distractors, task_family, epochs)
+    print("example", example)
+    print("objset", objset)
+    print("task", task)
+
+
+
+
+if __name__ == '__main__':
+  tf.compat.v1.app.run(main)
+
+
+
